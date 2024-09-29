c5e4b20dec043961aec09ca9d17bb0535db1e6e7
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-09-29 14:41:09 +0300

    Update main.go

--- hw1/calculator/main.go
+++ hw1/calculator/main.go
@@ -1,29 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"calculator/calc"
 	"fmt"
 	"os"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Введите строку: ")
-	for scanner.Scan() {
-
-		expression := scanner.Text()
-		expAnswer, err := calc.CalcExpr(expression)
-		if err == nil {
-			fmt.Println(expAnswer)
-		} else {
-			fmt.Println(err)
-		}
-		fmt.Print("Введите строку: ")
+	if len(os.Args) < 2 {
+		fmt.Println("Введите выражение.")
+		return
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Ошибка при чтении:", err)
+	expression := os.Args[1]
+	expAnswer, err := calc.CalcExpr(expression)
+	if err == nil {
+		fmt.Println(expAnswer)
+	} else {
+		fmt.Println(err)
 	}
-
 }
